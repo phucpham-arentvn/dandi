6d5a895b0b03c7644090b3a265d8231580c5f3da
phuc.pham <phuc.pham@arentvn.com>
2025-04-16 16:40:49 +0700

    Update Next.js configuration to allow images from GitHub and TutsPlus, and refactor Home page layout to enhance user experience with a new header, hero section, features, and pricing details.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: [
+      "lh3.googleusercontent.com",
+      "github.com",
+      "cms-assets.tutsplus.com",
+    ],
   },
 };
 
